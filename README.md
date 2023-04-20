22e215d7dbc7322a305e7283913a8011895c0a01
GustavoAG <GustavoTheProgrammer@gmail.com>
2023-04-20 18:06:35 -0400

    First version of Powerball API

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# powerball-article-generator
+# Powerball Article Generator API
+
+This API generates a news article about the Powerball lottery, including information about the most recent drawing and the next drawing.
+
+## Installation and Usage
+
+To use this API, you will need to have Python 3 installed on your local machine.
+
+1. Clone the GitHub repository:
+   ```
+   git clone https://github.com/[USERNAME]/powerball-article-generator.git
+   ```
+
+2. Install the required packages:
+   ```
+   pip install -r requirements.txt
+   ```
+
+3. Run the Flask application:
+   ```
+   export FLASK_APP=app.py
+   flask run
+   ```
+
+4. Access the API endpoint by navigating to `http://127.0.0.1:5000/powerball-article/` in your web browser or by sending an HTTP GET request to that URL.
+
+## API Endpoint
+
+### `GET /powerball-article/`
+
 This endpoint generates a news article about the Powerball lottery. The response will be in JSON format.
+
+#### Response Body
+
+A successful response will have a JSON object with the following properties:
+
+- `data`: an object with two properties:
+  - `headline`: a plain-text string indicating if there was a Powerball winner at the last drawing.
+  - `body`: an HTML string of the article body, including information about the most recent drawing and the next drawing.
+
+#### Example Request
+
+```
+GET /powerball-article/
+```
+
+#### Example Response
+
+```
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+  "data": {
+    "headline": "No Powerball winner at the last drawing",
+    "body": "<p>The Powerball lottery held its most recent drawing on Saturday, April 17th. Unfortunately, there was no winner of the jackpot prize. The winning numbers were 22, 36, 48, 55, 57, and the Powerball number was 24. Despite there being no jackpot winner, there were several winners of smaller prizes.</p><p>The next drawing will be held on Wednesday, April 21st. The jackpot prize for that drawing is estimated to be $90 million.</p>"
+  }
+}
+```
+
+## Key Decisions
+
+- Flask was chosen as the web framework for this API due to its simplicity and ease of use.
+- The `requests` library was used to make HTTP requests to the Powerball website to retrieve the most recent drawing information.
+- The `beautifulsoup4` library was used to parse the HTML response from the Powerball website and extract the necessary information.
+- The article body is generated dynamically based on the most recent drawing information and the date of the next drawing.
---
+++ app.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/powerball-article/', methods=['GET'])
+def powerball_article():
+    # Scrape the Powerball website to get the latest winning number
+    url = "https://www.powerball.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    winning_numbers = soup.select_one('div#numbers div.card-body div.row div.d-flex.col-auto.flex-nowrap.game-ball-group.mx-auto')
+
+    
+    # Get the winners data
+    winners_header = soup.select_one('div#winner div.card-body div.row.winner-card:nth-of-type(1) h5.card-title')
+    date = winners_header.text.strip()
+
+    jackpot_winners = soup.select_one('div#winner div.card-body div.row.winners-group:nth-of-type(1) span.winner-location').text.strip()
+
+    match_5_power_play_winners = soup.select_one('div#winner div.card-body div.row.winners-group:nth-of-type(2) span.winner-location').text.strip()
+
+    match_5_winners = soup.select_one('div#winner div.card-body div.row.winners-group:nth-of-type(3) span.winner-location').text.strip()
+
+
+
+    # Check if there was a Powerball winner
+    if "None" in jackpot_winners:
+        headline = "No Powerball Winner in the Last Drawing"
+        body = f"There was no Powerball winner in the last drawing, which took place on {date}."
+    else:
+        winner_state = jackpot_winners
+        headline = f"There was a Powerball Winner from the state of {winner_state} in the Last Drawing"
+        body = f"The winning Powerball numbers for the last drawing, which took place on {date}, were {winning_numbers}. The winning ticket was sold in {winner_state}."
+
+    # Get information about the next drawing
+    next_drawing_date = soup.select_one("#next-drawing .title-date").text.strip()
+    jackpot_amount = soup.select_one("#next-drawing .game-jackpot-number").text.strip()
+
+    # Build the article body
+    body += f" The next Powerball drawing will take place on {next_drawing_date}, with a potential prize of {jackpot_amount}."
+
+    # Build and return the response
+    response_data = {"headline": headline, "body": body}
+    return jsonify({"data": response_data})
+
+@app.route('/powerball-article/', methods=['POST'])
+def invalid_method():
+    # Return an error message for invalid HTTP method
+    return jsonify({
+        'error': 'Invalid HTTP method used. Please use GET method.'
+    }), 405
+
+@app.route('/powerball-numbers/')
+def powerball_numbers():
+    # Scrape the latest Powerball drawing data from data.ny.gov
+    url = 'https://data.ny.gov/resource/d6yy-54nr.json?$order=draw_date DESC&$limit=1'
+    response = requests.get(url)
+    data = response.json()[0]
+    numbers = data['winning_numbers']
+    powerball_number = data['powerball']
+
+    date = datetime.datetime.strptime(data['draw_date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%A, %B %d, %Y')
+
+    # Return the latest drawing date and numbers as a JSON response
+    response = jsonify({
+        'data': {
+            'date': date,
+            'numbers': numbers,
+            'powerball': powerball_number
+        }
+    })
+    response.headers.add('Content-Type', 'application/json')
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
